810c5e3c9a51ff2a850a2d3b62a4abe4357bb4ff
Thái Bảo <baont0209@gmail.com>
2022-08-12 14:46:05 +0700

    btn-menu

--- packages/pancake-uikit/src/widgets/Menu/Menu.tsx
+++ packages/pancake-uikit/src/widgets/Menu/Menu.tsx
@@ -163,7 +163,11 @@ const Menu: React.FC<NavProps> = ({
                 <MenuItems items={links} activeItem={activeItem} activeSubItem={activeSubItem} ml="12px" />
               )}
             </ContainerNav>
-            </StyledNav>
+            <BtnContainer alignItems="center" height="100%">
+              {userMenu}
+              {globalMenu}
+            </BtnContainer>
+          </StyledNav>
         </FixedContainer>
         {subLinks && (
           <Flex justifyContent="space-around">
